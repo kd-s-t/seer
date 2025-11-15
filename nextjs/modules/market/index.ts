3fe38698b35e79f507243017e4d8c941d8e531e3
Ken Dan Tinio <kendantinio@gmail.com>
2025-11-15 09:50:17 +0800

    Reorganize components: move module-specific components to their modules
    - Move MarketCard, MarketsList, MarketCreationForm to market module
    - Delete duplicate CryptoTable from components (market module has its own)
    - Delete duplicate NewsFeed and NewsCard from news module root
    - Update exports in components/index.ts and modules/market/index.ts
    - Keep only reusable components in components/ folder

--- nextjs/modules/market/index.ts
+++ nextjs/modules/market/index.ts
@@ -1,5 +1,8 @@
 export { default as CryptoTable } from './components/CryptoTable'
 export { default as MarketPage } from './components/MarketPage'
+export { default as MarketCard } from './components/MarketCard'
+export { default as MarketsList } from './components/MarketsList'
+export { default as MarketCreationForm } from './components/MarketCreationForm'
 export type { SortOption, SortDirection, SelectedReasoning, PendingStake } from './types'
 export * from './const'
 export * from './utils'
